607c93f63d725972ac16db3372cb1c9549b0adba
Cole Meldorf <cmeldorf@sas.upenn.edu>
2025-04-25 13:44:11 -0400

    Minor syntax issues

--- snappl/image.py
+++ snappl/image.py
@@ -4,6 +4,8 @@ from astropy.io import fits
 
 from snappl.logger import Lager
 
+from astropy.wcs import WCS as AstropyWCS
+
 class Exposure:
     pass
 
@@ -13,10 +15,10 @@ class OpenUniverse2024Exposure:
 
 class Image:
     """Cole did this (with software):
-                    ___                         
-                   / _ \___  __ _  ___ ____     
-                  / , _/ _ \/  ' \/ _ `/ _ \    
-                 /_/|_|\___/_/_/_/\_,_/_//_/    
+                    ___
+                   / _ \___  __ _  ___ ____
+                  / , _/ _ \/  ' \/ _ `/ _ \
+                 /_/|_|\___/_/_/_/\_,_/_//_/
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣔⣴⣦⣔⣠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⣿⣭⣿⣟⣿⣿⣿⣅⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣷⣾⣿⣿⣿⣿⣿⣿⣿⡶⠀⠀⠀⠀⠀⠀⠀⠀⠀
@@ -29,16 +31,16 @@ class Image:
 
                  _____  __     ___  __________
                 / __/ |/ /    / _ \/  _/_  __/
-               _\ \/    /    / ___// /  / /   
-              /___/_/|_/    /_/  /___/ /_/    
-                                
+               _\ \/    /    / ___// /  / /
+              /___/_/|_/    /_/  /___/ /_/
+
        """
-                                                 
+
     data_array_list = [ 'all', 'data', 'noise', 'flags' ]
-    
+
     def __init__( self, path, exposure, sca ):
         """type things here
-        
+
         Parameters
         ----------
           path : str
@@ -54,7 +56,7 @@ class Image:
             Roman.
 
         """
-        self.inputs = SimpleNamespace()
+        self.inputs = types.SimpleNamespace()
         self.inputs.path = path
         self.inputs.exposure = exposure
         self.inputs.sca = sca
@@ -98,16 +100,16 @@ class Image:
     def position_angle( self ):
         """Position angle in degrees east of north (or what)?"""
         raise NotImplementedError( f"{self.__class__.__name__} needs to implement position_angle" )
-        
+
     @property
     def image_shape( self ):
         """ny, nx pixel size of the image"""
         raise NotImplementedError( f"{self.__class__.__name__} needs to implement image_shape" )
-    
+
 
     def fraction_masked( self ):
         """Fraction of pixels that are masked."""
-        raise NotImplementedError( "Do.") 
+        raise NotImplementedError( "Do.")
 
     def get_data( self, which='all' ):
         """Read the data from disk and return one or more 2d numpy arrays of data.
@@ -131,7 +133,7 @@ class Image:
 
         """
         raise NotImplementedError( f"{self.__class__.__name__} needs to implement get_data" )
-    
+
 
     def get_wcs( self ):
         """Get an abstract WCS thingy
@@ -142,10 +144,10 @@ class Image:
 
         """
         raise NotImplementedError( f"{self.__class__.__name__} needs to implement get_wcs" )
-    
-    
+
+
     #     # THE REST OF THIS MAY GO AWAY
-        
+
     #     self.pipeline = pipeline
     #     self.logger = self.pipeline.logger
     #     self.sims_dir = pathlib.Path( os.getenv( 'SIMS_DIR', None ) )
@@ -217,7 +219,7 @@ class OpenUniverse2024FITSImage( Image ):
         self._noise = None
         self._flags = None
         self._wcs = None
-        
+
     @property
     def data( self ):
         if self._data is None:
@@ -227,26 +229,26 @@ class OpenUniverse2024FITSImage( Image ):
     def _load_data( self ):
         """Loads the data from disk."""
         raise NotImplementedError( "Do." )
-    
+
     def get_data( self, which='all' ):
         if which not in Image.data_array_list:
             raise ValueError( f"Unknown which {which}, must be all, data, noise, or flags" )
-        Lager.info( f"Reading FITS file {self.path}" )
-        with fits.open( self.path ) as hdul:
-            self._wcs = AstropyWCS.from_header( hdul[1].header )
+        Lager.info( f"Reading FITS file {self.inputs.path}" )
+        with fits.open( self.inputs.path ) as hdul:
+            self._wcs = AstropyWCS( hdul[1].header )
             if which == 'all':
-                return [ hdul[1].data, hdul[2].data, hdu[3].data ]
+                return [ hdul[1].data, hdul[2].data, hdul[3].data ]
             elif which == 'data':
-                return [ hdu[1].data ]
+                return [ hdul[1].data ]
             elif which == 'noise':
-                return [ hdu[2].data ]
+                return [ hdul[2].data ]
             elif which == 'flags':
-                return [ hdu[3].data ]
+                return [ hdul[3].data ]
             else:
                 raise RuntimeError( f"{self.__class__.__name__} doesn't understand data plane {which}" )
 
     def get_wcs( self ):
         if self._wcs is None:
-            with fits.open( self.path ) as hdul:
-                self._wcs = AstropyWCS.from_header( hdul[1].header )
+            with fits.open( self.inputs.path ) as hdul:
+                self._wcs = AstropyWCS( hdul[1].header )
         return self._wcs
