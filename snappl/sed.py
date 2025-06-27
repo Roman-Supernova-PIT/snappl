01f13aba2cd8d7d61bd5a24c3f63f2e5f7a49444
root <root@localhost>
2025-06-27 23:58:42 +0000

    Added tests which now pass

--- snappl/sed.py
+++ snappl/sed.py
@@ -38,39 +38,6 @@ class Single_CSV_SED( SED_collection ):
         return self.sed
 
 
-# Make a SED collection class or something
-
-# These should go somewhere else, but for now they're here.
-def ou24_open_parquet(parq, path, obj_type="SN", engine="fastparquet"):
-    """Convenience function to open a parquet file given its number."""
-    file_prefix = {"SN": "snana", "star": "pointsource"}
-    base_name = "{:s}_{}.parquet".format(file_prefix[obj_type], parq)
-    file_path = os.path.join(path, base_name)
-    df = pd.read_parquet(file_path, engine=engine)
-    return df
-
-
-def ou24_find_parquet(ID, path, obj_type="SN"):
-    """Find the parquet file that contains a given supernova ID."""
-
-    files = os.listdir(path)
-    SNLogger.debug(f"Looking for parquet files in {path} for {obj_type} with ID {ID}")
-    file_prefix = {"SN": "snana", "star": "pointsource"}
-    files = [f for f in files if file_prefix[obj_type] in f]
-    files = [f for f in files if ".parquet" in f]
-    files = [f for f in files if "flux" not in f]
-
-    for f in files:
-        pqfile = int(f.split("_")[1].split(".")[0])
-        df = ou24_open_parquet(pqfile, path, obj_type=obj_type)
-        # The issue is SN parquet files store their IDs as ints and star
-        # parquet files as strings.
-        # Should I convert the entire array or is there a smarter way to do
-        # this?
-        if ID in df.id.values or str(ID) in df.id.values:
-            return pqfile
-
-
 class OU2024_Truth_SED(SED_collection):
     def __init__(self, snid=None, isstar=False):
         # if (snid is None) or (sn_path is None):
@@ -83,13 +50,13 @@ class OU2024_Truth_SED(SED_collection):
 
         if isstar:
             self.lam_array, self.flambda_array = \
-                self.ou24_get_star_SED(snid, self.sn_path)
+                self._ou24_get_star_SED()
         else:
             self.lam_array, self.flambda_array, self.mjd_array = \
-                self.ou24_get_SN_SED(snid, self.sn_path)
+                self._ou24_get_SN_SED()
 
     def get_sed(self, snid=None, mjd=None):
-        """Return a galsim SED for the given snid and mjd.
+        """Return an SED for the given snid and mjd.
         Inputs:
         snid: the ID of the object
         mjd: the MJD of the observation (only used if this is a SN)
@@ -116,7 +83,33 @@ class OU2024_Truth_SED(SED_collection):
             # If this is a star, we just return the SED
             return np.array(self.lam_array), np.array(self.flambda_array)
 
-    def ou24_get_star_SED(self, SNID, sn_path):
+    def _ou24_open_parquet(self, parq, obj_type="SN", engine="fastparquet"):
+        """Convenience function to open a parquet file given its number."""
+        path = self.sn_path
+        file_prefix = {"SN": "snana", "star": "pointsource"}
+        base_name = "{:s}_{}.parquet".format(file_prefix[obj_type], parq)
+        file_path = os.path.join(path, base_name)
+        df = pd.read_parquet(file_path, engine=engine)
+        return df
+
+    def _ou24_find_parquet(self, obj_type="SN"):
+        """Find the parquet file that contains a given supernova ID."""
+        path = self.sn_path
+        ID = self.snid
+        files = os.listdir(path)
+        SNLogger.debug(f"Looking for parquet files in {path} for {obj_type} with ID {ID}")
+        file_prefix = {"SN": "snana", "star": "pointsource"}
+        files = [f for f in files if file_prefix[obj_type] in f]
+        files = [f for f in files if ".parquet" in f]
+        files = [f for f in files if "flux" not in f]
+
+        for f in files:
+            pqfile = int(f.split("_")[1].split(".")[0])
+            df = self._ou24_open_parquet(pqfile, obj_type=obj_type)
+            if ID in df.id.values or str(ID) in df.id.values:
+                return pqfile
+
+    def _ou24_get_star_SED(self):
         """Return the appropriate SED for the star.
         Inputs:
         SNID: the ID of the object
@@ -127,10 +120,10 @@ class OU2024_Truth_SED(SED_collection):
         flambda: the flux of the SED units in erg/s/cm^2/Angstrom
                 (numpy array of floats)
         """
-        filenum = ou24_find_parquet(SNID, sn_path, obj_type="star")
-        pqfile = ou24_open_parquet(filenum, sn_path, obj_type="star")
-        file_name = pqfile[pqfile["id"] == str(SNID)]["sed_filepath"].values[0]
-        # SED needs to move out to snappl
+
+        filenum = self._ou24_find_parquet(obj_type="star")
+        pqfile = self._ou24_open_parquet(filenum, obj_type="star")
+        file_name = pqfile[pqfile["id"] == str(self.snid)]["sed_filepath"].values[0]
         fullpath = pathlib.Path(Config.get().value("ou24." +
                                 "sims_sed_library")) / file_name
         sed_table = pd.read_csv(fullpath,  compression="gzip", sep=r"\s+",
@@ -139,7 +132,7 @@ class OU2024_Truth_SED(SED_collection):
         flambda = sed_table.iloc[:, 1]
         return np.array(lam), np.array(flambda)
 
-    def ou24_get_SN_SED(self, SNID, sn_path):
+    def _ou24_get_SN_SED(self):
         """Return the appropriate SED for the supernova on the given day.
 
         Inputs:
@@ -150,9 +143,9 @@ class OU2024_Truth_SED(SED_collection):
         lam: the wavelength of the SED in Angstrom
         flambda: the flux of the SED units in erg/s/cm^2/Angstrom
         """
-        filenum = ou24_find_parquet(SNID, sn_path, obj_type="SN")
+        filenum = self._ou24_find_parquet(obj_type="SN")
         file_name = "snana" + "_" + str(filenum) + ".hdf5"
-        fullpath = os.path.join(sn_path, file_name)
+        fullpath = os.path.join(self.sn_path, file_name)
         # Setting locking=False on the next line becasue it seems that you can't
         #   open an h5py file unless you have write access to... something.
         #   Not sure what.  The directory where it exists?  We won't
@@ -163,7 +156,7 @@ class OU2024_Truth_SED(SED_collection):
         #   think somebody else might change the file
         #   while you're in the middle of reading bits of it.
         sed_table = h5py.File(fullpath, "r", locking=False)
-        sed_table = sed_table[str(SNID)]
+        sed_table = sed_table[str(self.snid)]
         flambda = sed_table["flambda"]
         lam = sed_table["lambda"]
         mjd = sed_table["mjd"]
---
+++ snappl/tests/test_sed.py
@@ -0,0 +1,29 @@
+import pytest
+import pathlib
+import numpy as np
+
+from astropy.io import fits  # noqa: F401
+
+from snappl.sed import OU2024_Truth_SED
+
+
+def test_ou24_get_star_sed():
+    sed_obj = OU2024_Truth_SED(40120913)
+    lam, flux = sed_obj.get_sed(40120913, 62493)
+    sn_lam_test = np.load(pathlib.Path(__file__).parent
+                          / "testdata/SED/sn_lam_test.npy")
+    sn_flambda_test = np.load(pathlib.Path(__file__).parent
+                              / "testdata/SED/sn_flambda_test.npy")
+    np.testing.assert_allclose(lam, sn_lam_test, atol=1e-7)
+    np.testing.assert_allclose(flux, sn_flambda_test, atol=1e-7)
+
+
+def test_ou24_get_sn_sed():
+    sed_obj = OU2024_Truth_SED(40973149150, isstar=True)
+    lam, flux = sed_obj.get_sed(40973149150)
+    star_lam_test = np.load(pathlib.Path(__file__).parent
+                            / "testdata/SED/star_lam_test.npy")
+    star_flambda_test = np.load(pathlib.Path(__file__).parent
+                                / "testdata/SED/star_flambda_test.npy")
+    np.testing.assert_allclose(flux, star_flambda_test, atol=1e-7)
+    np.testing.assert_allclose(lam, star_lam_test, atol=1e-7)
