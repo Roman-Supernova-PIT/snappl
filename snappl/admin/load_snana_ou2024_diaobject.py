46417aa9fbdac18a47ce6d67bfb4a83bf7e0bd37
Rob Knop (Nersc) <raknop@lbl.gov>
2025-10-17 10:08:28 -0700

    Command-line utitilies for loading ou2024 objects and images

--- snappl/admin/load_snana_ou2024_diaobject.py
+++ snappl/admin/load_snana_ou2024_diaobject.py
@@ -1,6 +1,10 @@
 import re
 import uuid
 import simplejson
+import pathlib
+import functools
+import argparse
+import multiprocessing
 
 import pandas
 
@@ -9,7 +13,7 @@ from snappl.utils import SNPITJsonEncoder
 import snappl.db.db
 
 
-def load_snana_ou2024_diaobject( provid, pqf, dbcon=None ):
+def load_snana_ou2024_diaobject( provid, pqf, logevery=10000, dbcon=None ):
     match = re.search( r'^snana_([0-9]+)\.parquet$', pqf.name )
     if match is None:
         raise ValueError( f"Failed to parse filename {match}" )
@@ -18,7 +22,7 @@ def load_snana_ou2024_diaobject( provid, pqf, dbcon=None ):
 
     thingstoinsert = []
     for n, row in enumerate( df.itertuples() ):
-        if n % 1000 == 0:
+        if n % logevery == 0:
             SNLogger.info( f"File {pqf.name}, {n} of {len(df)} done" )
 
         params = { k: v for k, v in zip( row.model_param_names, row.model_param_values ) }
@@ -62,4 +66,82 @@ def load_snana_ou2024_diaobject( provid, pqf, dbcon=None ):
                    }
         thingstoinsert.append( subdict )
 
-    snappl.db.db.DiaObject.bulk_insert_or_upsert( thingstoinsert, dbcon=dbcon.con )
+    snappl.db.db.DiaObject.bulk_insert_or_upsert( thingstoinsert, dbcon=dbcon )
+
+    return len( thingstoinsert )
+
+
+def main():
+    parser = argparse.ArgumentParser( 'load_snana_ou2024_diaobject',
+                                      description='Load all parquet files below a directory.' )
+    parser.add_argument( '-p', '--provid', required=True, help="Provenance id" )
+    parser.add_argument( '-n', '--nprocs', type=int, default=12,
+                         help="Number of processes to run at once [default: 12]" )
+    parser.add_argument( 'basedir', help='Base directory.' )
+    args = parser.parse_args()
+
+    with snappl.db.db.DBCon( dictcursor=True ) as dbcon:
+        rows = dbcon.execute( "SELECT * FROM provenance WHERE id=%(id)s", { 'id': args.provid } )
+        if len(rows) == 0:
+            raise ValueError( "Invalid provenance {args.provid}" )
+        SNLogger.info( f"Loading provenance for process {rows[0]['process']} {rows[0]['major']}.{rows[0]['minor']}" )
+
+    # First, collect all the parquet files
+
+    def find_pqfiles( direc ):
+        pqfiles = []
+        subdirs = []
+        for f in direc.iterdir():
+            f = f.resolve()
+            if ( f.name[:5] == 'snana') and ( f.name[-8:] == '.parquet' ):
+                pqfiles.append( f )
+            elif f.is_dir():
+                subdirs.append( f )
+
+        for subdir in subdirs:
+            pqfiles.extend( find_pqfiles( subdir ) )
+
+        return pqfiles
+
+    basedir = pathlib.Path( args.basedir )
+    pqfiles = find_pqfiles( basedir )
+    SNLogger.info( f"{len(pqfiles)} parquet files to load" )
+    SNLogger.debug( f"All pqfiles we're going to do: {pqfiles}" )
+
+    # Launch parallel processes to load these files
+
+    did = []
+    tot = 0
+
+    def add_to_did( n ):
+        nonlocal tot, did
+        tot += n
+        did.append( n )
+        SNLogger.info( f"Completed {len(did)} of {len(pqfiles)} parquet files ({tot} objects)" )
+
+    errors = []
+
+    def omg( e ):
+        nonlocal errors
+        SNLogger.error( f"Subprocess returned error: {e}" )
+        errors.append( e )
+
+    do_load = functools.partial( load_snana_ou2024_diaobject, [ args.provid ] )
+
+    with multiprocessing.Pool( args.nprocs ) as pool:
+        for pqf in pqfiles:
+            pool.apply_async( do_load, [ pqf ], callback=add_to_did, error_callback=omg )
+        pool.close()
+        pool.join()
+
+    if len(errors) > 0:
+        nl = '\n'
+        SNLogger.error( f"There were errors:\n{nl.join(str(e) for e in errors)}" )
+        raise RuntimeError( "There were errors!" )
+
+    SNLogger.info( f"Loaded {tot} objects in {len(did)} parquet files." )
+
+
+# ======================================================================
+if __name__ == "__main__":
+    main()
