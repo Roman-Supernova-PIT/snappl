fca1d1407420e04ee79744eaeb96b417bdb3d7b2
Cole Meldorf <colefmeldorf@gmail.com>
2025-10-27 12:49:14 -0700

    Small bug fix

--- snappl/lightcurve.py
+++ snappl/lightcurve.py
@@ -393,7 +393,7 @@ class Lightcurve:
         """
 
         filetypemap = { 'parquet': 'parquet',
-                        'ecsv': 'ascii.escv'
+                        'ecsv': None
                        }
         if filetype not in filetypemap:
             raise ValueError( f"Unknown filetype {filetype}" )
