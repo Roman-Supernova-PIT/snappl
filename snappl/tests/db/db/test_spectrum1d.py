4aa2509a4d1a267d072c167a225065667326782a
Rob Knop (Nersc) <raknop@lbl.gov>
2025-11-05 13:32:09 -0800

    Create database spectrum1d table

---
+++ snappl/tests/db/db/test_spectrum1d.py
@@ -0,0 +1,42 @@
+import datetime
+import uuid
+import pytest
+
+from snappl.db.db import Spectra1d
+
+from basetest import BaseTestDB
+
+
+class TestSpectrum1d( BaseTestDB ):
+
+    @pytest.fixture
+    def basetest_setup( self, stupid_provenance, stupid_object ):
+        now = datetime.datetime.now( tz=datetime.UTC )
+        self.cls = Spectrum1d
+        self.safe_to_modify = [ 'filepath', 'epoch', 'created_at' ]
+        self.columns = set( self.safe_to_modify )
+        self.columns.update( [ 'id', 'provenance_id', 'diaobject_id', 'diaobject_position_id' ] )
+        self.uniques = []
+        self.obj1 = Spectrum1d( id=uuid.uuid4(),
+                                provenance_id=stupid_provenance,
+                                diaobject_id=stupid_object,
+                                filepath='/dev/null',
+                                epoch=60000000,
+                                created_at=now
+                               )
+        self.dict1 = { k: getattr( self.obj1, k ) for k in self.columns }
+        self.obj2 = Spectrum1d( id=uuid.uuid4(),
+                                provenance_id=stupid_provenance,
+                                diaobject_id=stupid_object,
+                                filepath='/bin/false',
+                                epoch=60001000
+                                created_at=now + datetime.timedelta( days=1 )
+                               )
+        self.dict2 = { k: getattr( self.obj2, k ) for k in self.columns }
+        self.dict3 = { 'id': uuid.uuid4(),
+                       'provenance_id': stupid_provenance,
+                       'diaobject_id': stupid_object,
+                       'filepath': '/bin/true',
+                       'epoch': 60002000
+                       'created_at': now + datetime.timedelta( days=2 )
+                      }
