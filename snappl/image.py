3cf552b34bda24692dec9520b53a212e42198f65
Rob Knop <rknop@pobox.com>
2025-11-18 15:17:18 -0500

    Try to make FITSImage.save not be the stupidest thing since banging your head against the wall

--- snappl/image.py
+++ snappl/image.py
@@ -1657,10 +1657,10 @@ class FITSImage( Numpy2DImage ):
         justwcshdr = None if wcshdr is None else FITSImage._astropy_header_to_fitsio_header( self._header )
         with fitsio.FITS( imagepath, 'rw' ) as f:
             f.write( self.data, header=imghdr )
-        if ( noisepath is not None ) and ( self.noise is not None ):
+        if saveno:
             with fitsio.FITS( noisepath, 'rw' ) as f:
                 f.write( self.noise, header=justwcshdr )
-        if ( self.flagspath is not None ) and ( self.flags is not None ):
+        if savefl:
             with fitsio.FITS( flagspath, 'rw' ) as f:
                 f.write( self.flags, header=justwcshdr )
 
