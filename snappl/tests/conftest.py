20dc4f863d1b24691e5e98a4b49d6ea4aad1152b
Michael Wood-Vasey <wmwv@pitt.edu>
2026-03-30 21:51:16 -0700

    Restore conftest.py and snappl_test_config.yaml.

--- snappl/tests/conftest.py
+++ snappl/tests/conftest.py
@@ -43,8 +43,7 @@ def output_directories():
 
 @pytest.fixture( scope='session', autouse=True )
 def init_config():
-#    Config.init( '/home/snappl/snappl/tests/snappl_test_config.yaml', setdefault=True )
-    Config.init( "/Users/wmwv/Roman/pipeline/snappl/snappl/tests/snappl_test_config.yaml", setdefault=True )
+    Config.init( '/home/snappl/snappl/tests/snappl_test_config.yaml', setdefault=True )
 
 
 @pytest.fixture( scope="session" )
