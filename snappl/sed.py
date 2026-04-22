71ffb46a1cb02757e81f7ff5e5386fa3bc5932b9
Cole Meldorf <colefmeldorf@gmail.com>
2026-04-22 14:06:37 -0700

    updated snappl SED

--- snappl/sed.py
+++ snappl/sed.py
@@ -32,9 +32,21 @@ class Flat_SED( SED_collection ):
 
 
 class Single_CSV_SED( SED_collection ):
-    def __init__( self, csv_file ):
-        # READ THE CSV FILE, make a galsim SED in self.sed
-        raise NotImplementedError( "Single_CSV_SED is not implemented yet.")
+    def __init__( self, csv_file, sed_wave_type = "Angstrom", sed_flux_type = "flambda" ):
+        """Initialize the SED collection with a single SED from a CSV file.
+        The CSV file should have two columns: the first column is the wavelength, and the second column is the flux.
+        Inputs:
+        csv_file: the path to the CSV file containing the SED
+        sed_wave_type: the type of the wavelength in the SED (default is "Angstrom")
+        sed_flux_type: the type of the flux in the SED (default is "flambda")
+
+        Returns:
+        galsim.SED: the initialized SED
+        """
+        import pdb; pdb.set_trace()
+        wave, flux = pd.read_csv(csv_file, comment="#", sep=r"\s+", header=None).values.T
+
+        self.sed = galsim.SED( csv_file, wave_type=sed_wave_type, flux_type=sed_flux_type )
 
     def get_sed( self, **kwargs ):
         return self.sed
