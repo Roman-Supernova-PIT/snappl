86d2a9588802db7fe0fad16dce533a2bf8f980f8
Rob Knop <raknop@lbl.gov>
2026-08-12 08:18:19 -0700

    Make this branch fail when people try to use it

--- snappl/__init__.py
+++ snappl/__init__.py
@@ -1,3 +1,5 @@
+raise RuntimeError( "You have the wrong branch of snappl checked out.  Check out u/rknop/2028photphest" )
+
 from importlib.metadata import version, PackageNotFoundError
 __all__ = []
 
